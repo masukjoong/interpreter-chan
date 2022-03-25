d1995ab754204b26c26f82c69025644e0b4561ea
himchannarae.kim <himchannarae.kim@navercorp.com>
2022-03-25 22:59:08 +0900

    Boolean literal Eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/masukjoong/interpreter-chan/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -15,6 +20,12 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		if node.Value {
+			return TRUE
+		}
+		return FALSE
 	}
 	return nil
 }
