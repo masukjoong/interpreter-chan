040f6f7fdd72a9516f2a6a0d1cd71d0dee29d13c
himchannarae.kim <himchannarae.kim@navercorp.com>
2022-04-30 07:38:04 +0900

    ast에 STRING 노드 추가

--- parser/parser.go
+++ parser/parser.go
@@ -65,6 +65,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IF, p.parseIfExpression)
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
+	p.registerPrefix(token.STRING, p.parseStringLiteral)
 
 	// register infix parse function
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -199,6 +200,13 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 
 	lit.Value = value
 	return lit
+}
+
+func (p *Parser) parseStringLiteral() ast.Expression {
+	return &ast.StringLiteral{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
